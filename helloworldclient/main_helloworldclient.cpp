e46ae33690a138ec4b24f9d66c7c31541f78abae
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-11-03 14:35:17 -0500

    #198 . added helloworld client code. It should be possible to access HelloWorldService from Java, without JNI, but we were never successful.

---
+++ helloworldclient/main_helloworldclient.cpp
@@ -0,0 +1,52 @@
+/*
+ * HelloWorld Service driver.
+ * Copyright (C) 2009 Michael Richardson <mcr@credil.org>
+ *
+ * Released under the terms of the file ../NOTICE
+ */
+#define LOG_TAG "HelloWorld/Service"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <grp.h>
+
+#include <utils/IPCThreadState.h>
+#include <utils/ProcessState.h>
+#include <utils/IServiceManager.h>
+#include <utils/Log.h>
+
+#include "helloworld.h"
+
+int main(int argc, char *argv[])
+{
+	LOGI("Hello client is now starting");
+
+        android::sp<android::IServiceManager> sm = android::defaultServiceManager();
+        android::sp<android::IBinder> binder;
+        android::sp<IHelloWorldClient> shw;
+
+        do {
+                binder = sm->getService(android::String16(HELLOWORLD_NAME));
+                if (binder != 0)
+                        break;
+                LOGW("HelloWorld not published, waiting...");
+                usleep(500000); // 0.5 s
+        } while(true);
+
+        //LOGI("sm: %u binder: %u", sm, binder);
+
+        //if (sDeathNotifier == NULL) {
+        //    sDeathNotifier = new DeathNotifier();
+        //}
+        //binder->linkToDeath(sDeathNotifier);
+        //sMediaPlayerService = interface_cast<IMediaPlayerService>(binder);
+
+	LOGI("Hello client is now trying");
+
+        shw = android::interface_cast<IHelloWorldClient>(binder);
+        shw->hellothere("fun");
+	
+	LOGI("Hello client is now exiting");
+
+	return(0);
+}
